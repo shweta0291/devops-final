249475d1b91f1619c01325edb59341511d66a1f2
shweta0291 <74100955+shweta0291@users.noreply.github.com>
2020-11-28 16:13:34 +0000

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','database-2.c0ioknt4akb6.us-east-1.rds.amazonaws.com');
+define('DB_USER','shweta');
+define('DB_PASS' ,'12345678');
 define('DB_NAME', 'onlinecourse');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
