687262907874628bf3112fa77f0cfa0efa47950f
shweta das <shwetadas@shwetas-MacBook-Air.local>
2020-11-28 18:56:00 +0000

    fixes

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    &copy; 2015 Online Course Registration | By : <a href="http://www.phpgurukul.com/" target="_blank">PHPGURUKUL</a>
+                    &copy; Dublin University Course Registration 
                 </div>
 
             </div>
